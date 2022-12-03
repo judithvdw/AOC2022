9d2bd255a1f7c80f64c2805d80e21c6938cf2af8
Judith <judithvdw@users.noreply.github.com>
2022-12-03 11:55:54 +0100

    day 3

--- 03.py
+++ 03.py
@@ -9,23 +9,18 @@ with open('inputs/3.txt') as f:
     backpacks = [i.strip() for i in f.readlines()]
 
 total = 0
-for backpack in backpacks:
-    length = len(backpack)
-    compartment1 = set(list(backpack[:length//2]))
-    compartment2 = set(list(backpack[length//2:]))
-    in_both = compartment1 & compartment2
-    shared = in_both.pop()
-    total += priority(shared)
+for b in backpacks:
+    compartment1 = set(list(b[:len(b)//2]))
+    compartment2 = set(list(b[len(b)//2:]))
+    in_both = (compartment1 & compartment2).pop()
+    total += priority(in_both)
 
 print(f"Part 1: {total}")
 
 total = 0
-for i in range(0,len(backpacks),3):
+for i in range(0, len(backpacks), 3):
     group = [set(list(b)) for b in backpacks[i:i+3]]
-    badge = group[0] & group[1] & group[2]
-    badge = badge.pop()
+    badge = (group[0] & group[1] & group[2]).pop()
     total += priority(badge)
 
 print(f"Part 2: {total}")
-
-
