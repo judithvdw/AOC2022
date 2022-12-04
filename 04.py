ad740446df8ad902b5481bcc100d2d030c76df0b
Judith <judithvdw@users.noreply.github.com>
2022-12-04 10:15:31 +0100

    day 4

---
+++ 04.py
@@ -0,0 +1,20 @@
+import re
+
+
+def contains(assignment):
+    return assignment[0] <= assignment[2] and assignment[1] >= assignment[3] or \
+           assignment[0] >= assignment[2] and assignment[1] <= assignment[3]
+
+
+def overlap(assignment):
+    return not (assignment[0] < assignment[2] and assignment[1] < assignment[2] or
+                assignment[0] > assignment[3] and assignment[1] > assignment[3])
+
+
+with open("inputs/4.txt") as f:
+    m = re.compile("(\d+)-(\d+),(\d+)-(\d+)")
+    assignments = [list(map(int, m.match(i).groups())) for i in f.readlines()]
+
+    print(f"Part 1: {sum(map(contains, assignments))}")
+    print(f"Part 2: {sum(map(overlap, assignments))}")
+
