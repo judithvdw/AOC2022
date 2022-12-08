2c95e836ad9956d930e23fab71a8c753f4f39d65
Judith <judithvdw@users.noreply.github.com>
2022-12-08 20:08:44 +0100

    day 8 part 2

--- 08.py
+++ 08.py
@@ -2,6 +2,7 @@ with open("inputs/8.txt") as f:
     forest = [list(map(int, list(i.strip()))) for i in f.readlines()]
     transposed_forest = list(map(list, zip(*forest)))
 
+
 visible = 0
 for i, row in enumerate(forest):
     for j in range(len(row)):
@@ -11,4 +12,15 @@ for i, row in enumerate(forest):
                 all(transposed_forest[j][i] > b for b in transposed_forest[j][:i]):
             visible += 1
 
-print(visible)
+print(f"Part 1: {visible}")
+
+scenic_scores = []
+for i, row in enumerate(forest):
+    for j, tree in enumerate(row):
+        up = next((i + 1 for i, val in enumerate(transposed_forest[j][:i][::-1]) if val >= tree), len(transposed_forest[j][:i]))
+        down = next((i + 1 for i, val in enumerate(transposed_forest[j][i + 1:]) if val >= tree), len(transposed_forest[j][i + 1:]))
+        left = next((i + 1 for i, val in enumerate(row[:j][::-1]) if val >= tree), len(row[:j]))
+        right = next((i + 1 for i, val in enumerate(row[j + 1:]) if val >= tree), len(row[j + 1:]))
+        scenic_scores.append(up * down * left * right)
+
+print(f"Part 2: {max(scenic_scores)}")
